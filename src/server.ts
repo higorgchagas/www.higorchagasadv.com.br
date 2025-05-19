8a011029d420d543f5a46aa7f74329850cd49b33
Higor Chagas <higorgchagas@gmail.com>
2025-05-19 17:54:53 -0300

    alteração site

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`🚀 Servidor rodando em http://localhost:${port}`);
+});
